82a2bb63294cc484728ae7da3c7ee2bc48f21dc7
Hồ Tá Quý <taquyit@gmail.com>
2017-05-07 17:06:38 +0700

    update connect

--- connect.php
+++ connect.php
@@ -1,6 +1,6 @@
 <?php
 function  getConnect(){
-	$conn= mysqli_connect('localhost','root','','dbhoian') 
+	$conn= mysqli_connect('127.0.0.1:50810','azure','6#vWHD_$','dbhoian') 
 	or
 	die("No connect!");
 	mysqli_query($conn,"SET character_set_results=utf8");
