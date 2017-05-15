7cf126e92ab408ceacdf18b714ea58b6876e2340
xoapit <taquyit@gmail.com>
2017-05-15 10:23:24 +0700

    edit footer

---
+++ footer.php
@@ -0,0 +1,32 @@
+<div style="background: #2c3e50; color:#fefefe;">
+	<div class="container">
+		
+		<div class="col-md-12">
+			<h3 style="border-bottom:5px black solid; padding-bottom: 5px;">HỘI AN TRONG TÔI</h3>
+		</div>
+		<div class="row">
+			<div class="col-md-12"><h4>LIÊN HỆ</h4></div>
+			<div class="col-md-6">
+				<h4 style="font-weight: bold;">Đinh Hữu Quân</h4>
+				<p>Email: huuquan95@gmail.com</p>
+				<p>Phone: 0979432236</p>
+				<p>Địa chỉ: Quảng Nam</p>
+			</div>
+			<div class="col-md-6">
+				<h4 style="font-weight: bold;">Hồ Tá Quý</h4>
+				<p>Email: xoapit@gmail.com</p>
+				<p>Phone: 01672734732</p>
+				<p>Địa chỉ: Huế</p>
+			</div>
+		</div>
+		<div class="col-md-12">
+		</div>
+	</div>
+	<div class="row" style="background-color: #2c2e40;">
+		<div class="container">
+			<div class="col-md-3"><p style="font-weight: bold; margin: 10px 0 10px;">@Copyright by QQTeam 2017</p></div>
+			<div class="col-md-9"><p style="font-weight: bold; margin: 10px 0 10px;"><marquee style="">LATER EQUALS NEVER</marquee></p></div>
+
+		</div>
+	</div>	
+</div>
