7780a86cd29f8f957f34778814ae39708839a1b1
huuquan95 <huuquan95@gmail.com>
2017-05-21 18:03:46 +0700

    Update index.php

--- index.php
+++ index.php
@@ -11,7 +11,7 @@ include('header.php');
 			$articles= getListArticles();
 			int $i=0;
 			foreach ($articles as $article) {
-			if($i++=10) break;
+			if($i++=1) break;
 			?>							
 				<div>
 					<div class="col-md-12">
