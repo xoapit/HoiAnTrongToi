1b0e06ea859820b49c0e7fa877c95f3396f06ad7
xoapit <taquyit@gmail.com>
2017-05-21 10:56:13 +0700

    ckfinder

--- ckeditor/config.js
+++ ckeditor/config.js
@@ -3,8 +3,11 @@
  * For licensing, see LICENSE.md or http://ckeditor.com/license
  */
 
-CKEDITOR.editorConfig = function( config ) {
-	// Define changes to default configuration here. For example:
-	// config.language = 'fr';
-	// config.uiColor = '#AADC6E';
-};
+ CKEDITOR.editorConfig = function( config ) {
+ 	config.filebrowserBrowseUrl = ‘../ckfinder/ckfinder.html’;
+ 	config.filebrowserImageBrowseUrl = ‘../ckfinder/ckfinder.html?type=Images’;
+ 	config.filebrowserFlashBrowseUrl = ‘../ckfinder/ckfinder.html?type=Flash’;
+ 	config.filebrowserUploadUrl = ‘../ckfinder/core/connector/php/connector.php?command=QuickUpload&type=Files’;
+ 	config.filebrowserImageUploadUrl = ‘../ckfinder/core/connector/php/connector.php?command=QuickUpload&type=Images’;
+ 	config.filebrowserFlashUploadUrl = ‘../ckfinder/core/connector/php/connector.php?command=QuickUpload&type=Flash’;
+ };
---
+++ ckfinder/config.js
@@ -0,0 +1,10 @@
+/*
+ Copyright (c) 2007-2016, CKSource - Frederico Knabben. All rights reserved.
+ For licensing, see LICENSE.html or http://cksource.com/ckfinder/license
+ */
+
+var config = {};
+
+
+
+CKFinder.define( config );
