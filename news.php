e7c7a6979a0a4cf22fcb13c75720113a1576db53
xoapit <taquyit@gmail.com>
2017-05-26 10:45:22 +0700

    news

--- news.php
+++ news.php
@@ -13,7 +13,7 @@
 					<img src="<?php echo $article->getImage(); ?>" style="height: 70px; width: 70px; margin-top:-10px;padding-top: 0px;">
 				</dir>
 				<div class="col-md-9">
-					<p><a href="viewArticle.php?idArticle=<?php echo $article->getIdArticle(); ?>"><?php echo $article->getTitle(); ?></a></p>
+					<p><a href="viewArticle.php?idArticle=<?php echo $article->getIdArticle(); ?>&idCategory=<?php echo $article->getIdCategory(); ?>"><?php echo $article->getTitle(); ?></a></p>
 				</div>
 				<hr>
 			</div>
